c32c2697af5a3673a2645d6bdfb4df39701f2945
Zoe77 <audreylaly03@gmail.com>
2025-07-05 19:47:20 +0100

    feat: create table and seeder of sitetouristique

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\SiteTouristiqueController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('index', [SiteTouristiqueController::class, 'index'])->name('index');
